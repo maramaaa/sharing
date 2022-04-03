6af8d39e822a737dd795c5f9c74042a4d8492df8
maramaaa <94992807+maramaaa@users.noreply.github.com>
2022-04-03 14:06:40 +0300

    Create task_1.cpp

---
+++ task_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(){
+    int am[11]={0};
+    int t, n, k;
+    string s;
+    cin >> t;
+    bool b = 1;
+    for (int test = 0; test < t; test++){
+        cin >> n >> k;
+        for (int num = 0; num < n; num++) {
+            cin >> s;
+            for (int i = 0; i < k; i++) {
+                if (s[i] == '1') am[i]++;
+            }
+        }
+        for (int i = 0; i < n; i++){
+            if (am[i] % 2 == 1) {cout << "NO" << endl; b = 0;}
+        }
+        if (b) cout << "YES" << endl;
+        int am[11];
+    }
+}
